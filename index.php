e338024ecdd2babd920cd7b0b602d855df18ca43
Sheenam3 <45314018+Sheenam3@users.noreply.github.com>
2018-11-24 20:56:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+        ?>
+        <a href="login.php"> Click here to login <br/>
+        <a href="register.php"> Click here to register <br/>
+        <a href="forgot.php"> Forgot Password? <br/>
+
+    </body>
+</html>
